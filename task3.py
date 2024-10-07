8fc1c1fec6be036f9da7b56d84fb271e2d5a4c14
kirill671games <kirill6040@gmail.com>
2024-10-07 20:04:49 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,55 +1,16 @@
-a = int(input("Введите число от 1 до 12: "))
-b = input("ru or en? ")
-def month(a, b):
-    if b == "en":
-        if a == 1:
-            result = "January"
-        elif a == 2:
-            result = "February"
-        elif a == 3:
-            result = "March"
-        elif a == 4:
-            result = "April"
-        elif a == 5:
-            result = "May"
-        elif a == 6:
-            result = "June"
-        elif a == 7:
-            result = "July"
-        elif a == 8:
-            result = "August"
-        elif a == 9:
-            result = "September"
-        elif a == 10:
-            result = "October"
-        elif a == 11:
-            result = "November"
-        elif a == 12:
-            result = "December"
-    elif b == "ru":
-        if a == 1:
-            result = "Январь"
-        elif a == 2:
-            result = "Февраль"
-        elif a == 3:
-            result = "Март"
-        elif a == 4:
-            result = "Апрель"
-        elif a == 5:
-            result = "Май"
-        elif a == 6:
-            result = "Июнь"
-        elif a == 7:
-            result = "Июль"
-        elif a == 8:
-            result = "Август"
-        elif a == 9:
-            result = "Сентябрь"
-        elif a == 10:
-            result = "Октябрь"
-        elif a == 11:
-            result = "Ноябрь"
-        elif a == 12:
-            result = "Декабрь"
-    print(f'{result}')
-month(a, b)
+def month(month_number, language):
+    if month_number < 1 or month_number > 12:
+        return "Некорректный номер месяца"
+    months = {
+        'ru': [
+            "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+            "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+        ],
+        'en': [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ]
+    }
+    return months[language][month_number - 1]
+
+print(month(1, 'ru'))
